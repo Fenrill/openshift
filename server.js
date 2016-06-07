6469e5f9cac16198387feb9e49bf1540b2d55623
Ricardo Estrada <Ricardo Estrada>
2016-06-07 02:46:38 -0600

    added web service endpoints findAll and findbyId for entity developer

--- server.js
+++ server.js
@@ -1,6 +1,22 @@
 var express = require('express');
 var app = express();
 
+var developer = [
+    {firstName: 'Alice', lastName: 'Wonderland'},
+    {firstName: 'Bob', lastName: 'Marley'},
+    {firstName: 'Charlie', lastName: 'Garcia'},
+    {firstName: 'Dan', lastName: 'Aykroyd'},
+]
+
+app.get('/rest/developer', function(req, res){
+    res.json(developer);
+})
+
+
+app.get('/rest/developer/:index', function(req, res){
+    res.json(developer[req.params.index]);
+})
+
 app.use(express.static(__dirname + '/public'));
 
 var ip = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
