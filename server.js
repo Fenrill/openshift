77e490447ae781f06203eaa7cf25474d53db05d2
Ricardo Estrada <Ricardo Estrada>
2016-06-08 02:38:29 -0600

    added dependencies with body parser and multer, and created creat web server, endpoint to create new developers

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+var multer = require('multer');
+var upload = multer();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 var developer = [
     {firstName: 'Alice', lastName: 'Wonderland'},
@@ -10,6 +16,12 @@ var developer = [
 
 app.delete('/rest/developer/:index', function(req, res){
     developer.splice(req.params.index, 1);
+    res.json(developer);
+})
+
+app.post('/rest/developer/:index', function(req, res){
+    var newDeveloper = req.body;
+    developer.push(newDeveloper);
     res.json(developer);
 })
 
